f8baf6af5bdbbf7d9017db2999dc503e0dc22673
LeHoang510 <lehoangnguyen510@gmail.com>
2022-07-15 06:02:08 +0200

    Implement advanced login

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -50,5 +50,7 @@ class UserTest < ActionDispatch::IntegrationTest
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
-
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
 end
